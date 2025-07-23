f556c49ea3025723eb04d8be378a0c61e394c7df
JPgitidaho <juanichile2015@gmail.com>
2025-07-23 15:25:55 -0400

    actualization

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Tasks from './pages/Tasks'
-import styles from './styles/App.module.css'
-
+import appStyles from './styles/App.module.css' 
 
 function App() {
   const isAuthenticated = !!localStorage.getItem('token')
 
   return (
-    <div className={styles.container}>
+    <div className={appStyles.container}> 
       <Routes>
         <Route path="/" element={isAuthenticated ? <Tasks /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
